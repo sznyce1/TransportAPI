4a0e6130a55fd68eef60dee4478c9c0f913de499
sznyce1 <98336306+sznyce1@users.noreply.github.com>
2024-05-03 04:30:31 +0200

    Validation for CreateRunDto

--- TransportAPI/Models/UpdateRunDto.cs
+++ TransportAPI/Models/UpdateRunDto.cs
@@ -1,12 +1,19 @@
-﻿namespace TransportAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TransportAPI.Models
 {
     public class UpdateRunDto
     {
+        [Required]
         public int CarId { get; set; }
+        [Required]
         public int DriverId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
+        [Required]
+        [Range(0, 99999)]
         public double Distance { get; set; }
+        [Range(0, 100)]
         public double? AverageFuelConsumption { get; set; }
     }
 }
